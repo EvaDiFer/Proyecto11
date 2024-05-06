084c277780eb246c86c9eaebc8fe100391e0499a
EvaDiFer <evadiazfer16@gmail.com>
2024-05-06 18:23:35 +0200

    change img

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const getImageUrl = (path) => {
   if (!path) {
-    return 'https://static.vecteezy.com/system/resources/previews/006/059/989/non_2x/crossed-camera-icon-avoid-taking-photos-image-is-not-available-illustration-free-vector.jpg'; // URL de una imagen de marcador de posición
+    return 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png';
   }
   return `https://image.tmdb.org/t/p/w500${path}`;
 };
